ff9af7b2c1051929a818d31b92e42f6bafead432
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-06-26 09:41:25 +0200

    And initial login code, fully migrate to standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+// import { MatCardModule } from "@angular/material/card";
+// import { MatSidenavModule } from "@angular/material/sidenav";
+// import { MatCheckboxModule } from "@angular/material/checkbox";
+import { SalesComponent } from './tabs/sales/sales.tab';
+import { AccountsComponent } from './tabs/accounts/accounts.tab';
+import { ProductsComponent } from './tabs/products/products.tab';
+import { LoginComponent } from './login/login.component';
+// import { AppRoutingModule } from "./app.routes";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatTabsModule,
+    SalesComponent,
+    AccountsComponent,
+    ProductsComponent
+    //LoginComponent,
+    //     MatCardModule,
+    //     MatSidenavModule,
+    //     MatCheckboxModule,
+
+    //     AppRoutingModule
+  ]
 })
 export class AppComponent {
-  title = 'Frontend';
-
+  title = 'Byte Bazaar';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { AppModule } from './app/app.module'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { AppModule } from './app/app.module'
+// import { AppComponent } from './app/app.component';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-platformBrowserDynamic()
-      .bootstrapModule(AppModule)
-      .catch((err) => console.error('Problem bootstrapping angular project', err));
+// platformBrowserDynamic()
+//       .bootstrapModule(AppModule)
+//       .catch((err) => console.error('Problem bootstrapping angular project', err));
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig).catch(err =>
+  console.error('Problem bootstrapping angular project', err)
+);
