7b02248ed68dbc92633cd8460c672a528dd05597
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-06-26 19:26:02 +0200

    removed old code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-// import { AppModule } from './app/app.module'
-// import { AppComponent } from './app/app.component';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-// platformBrowserDynamic()
-//       .bootstrapModule(AppModule)
-//       .catch((err) => console.error('Problem bootstrapping angular project', err));
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
