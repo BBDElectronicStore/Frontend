d4c4826c70f6a37ae9302a304028ecaec363f5c3
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-07-02 20:53:12 +0200

    Removed all local host redirects

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const devEnvironment = {
     baseURL: "",
-    cognitoURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=2r9cieeqb6k9k441h6vnkch4te&response_type=code&identity_provider=Google&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fhome",
+    cognitoURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=2r9cieeqb6k9k441h6vnkch4te&response_type=code&identity_provider=Google&scope=email+openid+phone&redirect_uri=https://electronics.projects.bbdgrad.com",
     tokenURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/oauth2/token",
-    logoutURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/logout?client_id=2r9cieeqb6k9k441h6vnkch4te&logout_uri=http://localhost:4200",
+    logoutURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/logout?client_id=2r9cieeqb6k9k441h6vnkch4te&logout_uri=https://electronics.projects.bbdgrad.com",
     clientID: "2r9cieeqb6k9k441h6vnkch4te",
     redirectURI: "https://electronics.projects.bbdgrad.com/home",
     userInfoURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/oauth2/userInfo"
