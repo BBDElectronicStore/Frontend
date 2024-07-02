1b1835e6af5bc50f1a672f70b9b524baa1cd60ad
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-07-02 20:41:08 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,6 @@ export const devEnvironment = {
     tokenURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/oauth2/token",
     logoutURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/logout?client_id=2r9cieeqb6k9k441h6vnkch4te&logout_uri=http://localhost:4200",
     clientID: "2r9cieeqb6k9k441h6vnkch4te",
-    redirectURI: "http://electronics.projects.bbdgrad.com/home",
+    redirectURI: "https://electronics.projects.bbdgrad.com/home",
     userInfoURL: "https://byte-bazaar.auth.eu-west-1.amazoncognito.com/oauth2/userInfo"
 };
